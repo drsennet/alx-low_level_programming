da0964cbd2e9afe29915c6c2f8e3bc90ea09aec8
drsennet <drsennet@gmail.com>
2023-06-17 05:58:01 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * File: 5-printfs.c
+ * Auth: Mamudu Hamidu <drsennet@gmail.com>
+ */
+#include <stdio.h>
+/**
+ * main - printf with proper grammar, but the outcome is a piece of art,
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
