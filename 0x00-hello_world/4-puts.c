f8e0d515c0179f73f6de36ff337c31a1544b5d2c
drsennet <drsennet@gmail.com>
2023-06-17 05:19:17 -0700

    puts stdout function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	/* initializing strings*/
-	char str1[] = "\"Programming is like building a multilingual puzzle\n";
-	/* writing to stdout*/
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
