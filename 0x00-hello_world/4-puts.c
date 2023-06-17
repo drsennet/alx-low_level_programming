d5e5eb670defcb0aba394db8a46cd06ba32f1422
drsennet <drsennet@gmail.com>
2023-06-17 05:45:47 -0700

    puts stdout function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Auth: Mamudu Hamidu <drsennet@gmail.com>
+ */
 #include <stdio.h>
+/**
+ * main - puts Programming is like a multilingual
+ *		puzzle, followed by a new line.
+ *
+ *Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
