6b7acf90cc1e160c5ba65d90377a279fcf1deda1
drsennet <drsennet@gmail.com>
2023-06-22 10:08:49 -0700

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+/*
+ * File: 2-print_alphabet.c
+ * Author: Mamudu Hamidu <drsennet@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase,
+ *		using putchar.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char ch;
+
+	/*print lowercase alphabets*/
+	for (ch = 'a'; ch <= 'z'; ch++)
+	
+		putchar(ch);
+
+	putchar('\n');
+	
+
+	return (0);
+}
