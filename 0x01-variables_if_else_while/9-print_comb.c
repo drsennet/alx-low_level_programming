cb75c552a2243a5bf6d490eaea1276e5e5e9ce8a
drsennet <drsennet@gmail.com>
2023-06-22 12:42:14 -0700

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ * File: 9-print_com.c
+ * Author: Mamudu Hamidu <drsennet@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible,
+ *		combinations of single-digit numbers using putchar.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	/*print*/
+	for (i = 0; i < 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
