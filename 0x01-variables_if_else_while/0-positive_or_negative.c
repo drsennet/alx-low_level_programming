f04c7d499752f4f5fad72a2eec206b1f7b5e5cf0
drsennet <drsennet@gmail.com>
2023-06-22 06:34:59 -0700

    0-positive_or_negative_zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,42 @@
 /**
-*File: 0-positive_or_negative.c
-* Author: Mamudu Hamidu <drsennet@gmail.com>
-* /
-#include <stdlib.h> 
-#include <time.h> 
- /* more headers goes there */ 
-  
- /* betty style doc for function main goes there */ 
- int main(void) 
- { 
-         int n; 
-  
-         srand(time(0)); 
-         n = rand() - RAND_MAX / 2; 
-         /* your code goes there */ 
-         if (n > 0)
-          {
-                  /* Write stdio for n as positive*/
-                   printf("%d is positive\n" );
-           }
-            else
-           {
-                   /* Write stdio for n as negative tive*/
-                    printf("%d is negative\n" );
-              }
-         return (0); 
- }
+ * File: 0-positive_or_negative.c
+ * Author: Mamudu Hamidu <drsennet@gmail.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare variable.*/
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		/* Write stdio for n as positive*/
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		/* Write stdio for n as negative tive*/
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		/* if none*/
+		printf("%d is zero\n", n);
+	}
+	/* Return 0.*/
+	return (0);
+}
